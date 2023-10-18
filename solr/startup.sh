4a7946c4011aed407087ab924681dae880b4e1a9
Fábio Sá <up202007658@edu.fe.up.pt>
2023-10-18 14:49:23 +0100

    Milestone 2: fun with solr

--- solr/startup.sh
+++ solr/startup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# This script expects a container started with the following command.
-docker run -p 8983:8983 --name meic_solr -v ${PWD}:/data -d solr:9.3 solr-precreate courses
+# Runing Solr container
+# Creating a project "privago"
+# Creating a virtual folder "data"
+docker run -p 8983:8983 --name privago -v .:/data -d solr:9.3
 
-# Schema definition via API
-curl -X POST -H 'Content-type:application/json' \
-    --data-binary "@./simple_schema.json" \
-    http://localhost:8983/solr/courses/schema
+# Creating a schema based on "schema.json" file into "privago" project
+curl -X POST -H 'Content-type:application/json' --data-binary "@./schema.json" http://localhost:8983/solr/privago/schema
 
-# Populate collection using mapped path inside container.
-docker exec -it meic_solr bin/post -c courses /data/meic_courses.json
+# Copy pipeline output to virtual "data" folder
+cp ../data/processed/hotels_complete.json .
+
+# Create "hotels" core inside "privago" project"
+# Populate collection using mapped folder "data" and pipeline output
+docker exec -it privago bin/post -c hotels /data/hotels_complete.json
+
+# Removing always pipeline output for github/commits reasons
+rm hotels_complete.json
