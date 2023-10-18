5d592dc368d005e892294243c7c048b0d560bd16
Fábio Sá <up202007658@edu.fe.up.pt>
2023-10-18 15:54:00 +0100

    Milestone 2: solr setup script

--- solr/startup.sh
+++ solr/startup.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# Runing Solr container
-# Creating a project "privago"
-# Creating a virtual folder "data"
-docker run -p 8983:8983 --name privago -v .:/data -d solr:9.3
-
-# Creating a schema based on "schema.json" file into "privago" project
-curl -X POST -H 'Content-type:application/json' --data-binary "@./schema.json" http://localhost:8983/solr/privago/schema
+# To avoid problems with duplicated containers
+docker stop privago
+docker rm privago
 
 # Copy pipeline output to virtual "data" folder
 cp ../data/processed/hotels_complete.json .
 
-# Create "hotels" core inside "privago" project"
+# Running Solr container
+# Creating "privago"
+# Creating a virtual folder "data"
+# Creating "hotels" core/collection
+docker run -p 8983:8983 --name privago -v .:/data -d solr:9.3 solr-precreate hotels
+sleep 3
+
+# Creating a schema based on "schema.json" file into "privago"
+curl -X POST -H 'Content-type:application/json' --data-binary "@./schema.json" http://localhost:8983/solr/hotels/schema
+sleep 1
+
 # Populate collection using mapped folder "data" and pipeline output
 docker exec -it privago bin/post -c hotels /data/hotels_complete.json
+sleep 1
 
 # Removing always pipeline output for github/commits reasons
 rm hotels_complete.json
