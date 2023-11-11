78934265b5bc19255820db6040c6b97b2ae4f007
Fábio Sá <up202007658@edu.fe.up.pt>
2023-11-11 12:37:13 +0000

    fix flow

---
+++ solr/startup_schemaless.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# To avoid problems with duplicated containers
+docker stop privago
+docker rm privago
+
+# Copy pipeline output to virtual "data" folder
+cp ../data/processed/hotels_complete.json .
+
+# Running Solr container
+# Creating "privago"
+# Creating a virtual folder "data"
+# Creating "hotels" core/collection
+docker run -p 8983:8983 --name privago -v "$(pwd)":/data -d solr:9.3 solr-precreate hotels
+sleep 3
+
+# Populate collection using mapped folder "data" and pipeline output
+docker exec -it privago bin/post -c hotels -format solr /data/hotels_complete.json
+sleep 1
+
+# Removing always pipeline output for github/commits reasons
+rm hotels_complete.json
+
+# Query: qt=/select & q=name:* & fl=*,[child] & indent on
