606f7eab81cadb35a5bee453a652d09afb557ea0
Fábio Sá <up202007658@edu.fe.up.pt>
2023-11-11 13:18:43 +0000

    precision at k

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,20 +1,31 @@
 import json
 
+PRECISION_AT = 20
+
 def getResults(query: int, mode: str) -> list:
     path = f"./q{query}/evaluation-{mode}.json"
     with open(path, 'r') as file:
         data = json.load(file)
         file.close()
     
-    return list(data.values())
+    results = data.values()
+    assert len(results) == 60
+    return list(results)
+
+def precision_at_k(results: list) -> float:
+    return len([
+        result for result in results[:PRECISION_AT] if result == 1
+    ]) / PRECISION_AT
+
+def average_precision(results: list) -> float:
+    return 10
 
 def evaluate(query: int, mode: str) -> None:
     results = getResults(query, mode)
-    assert len(results) == 60
-    print(results)
+    print(precision_at_k(results))
 
 if __name__ == "__main__":
 
     for mode in ['schemaless', 'boosted']:
-        for index in range(1, 2): # ..5, only q1 for development/debug reasons
-            evaluate(index, mode)
+        for query in range(1, 2): # ..5, only q1 for development/debug reasons
+            evaluate(query, mode)
