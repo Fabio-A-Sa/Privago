e035ec2e399c8e8d8f54d7ec442a30e7095ff655
Fábio Sá <up202007658@edu.fe.up.pt>
2023-11-11 14:33:55 +0000

    recall

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,5 +1,6 @@
 import json
 
+LIMIT = 60
 PRECISION_AT = 20
 
 def getResults(query: int, mode: str) -> list:
@@ -9,7 +10,7 @@ def getResults(query: int, mode: str) -> list:
         file.close()
     
     results = data.values()
-    assert len(results) == 60
+    assert len(results) == LIMIT
     return list(results)
 
 # P@K
@@ -18,19 +19,43 @@ def precision_at_k(results: list, k: int = PRECISION_AT) -> float:
         result for result in results[:k] if result == 1
     ]) / k
 
+def recall_at_k(results: list, k: int) -> float:
+    return len([
+        result for result in results[:k] if result == 1
+    ]) / len(results)
+
+def precision_values(results: list) -> float:
+    return [
+        precision_at_k(results, k) for k in range(1, len(results)+ 1)
+    ]
+
+def recall_values(results: list) -> float:
+    return [
+        recall_at_k(results, k) for k in range(1, len(results)+ 1)
+    ]
+
+# MAP
+def mean_average_precision() -> float:
+    # interpretar a necessidade desta métrica no contexto global
+    pass
+
 # AvP
 def average_precision(results: list) -> float:
-    precisions = 0
-    for index in range(0, len(results)):
-        precisions += precision_at_k(results, index + 1)
+    precisions = precision_values(results)
+    return round(sum(precisions) / len(results), 2)
 
-    return round(precisions / len(results), 2)
+# Recall
+def recall(results: list) -> float:
+    return round(sum(recall_values(results)), 2)
+
+
 
 def evaluate(query: int, mode: str) -> None:
     results = getResults(query, mode)
     print(f"Mode: {mode}")
     print(f"P@20: {precision_at_k(results)}")
     print(f"AvP: {average_precision(results)}")
+    print(f"Recall: {recall(results)}")
 
 if __name__ == "__main__":
 
