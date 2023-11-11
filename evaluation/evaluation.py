0fa65a5cf7d5f22f5f870481e2c8cf5440b08ec1
Fábio Sá <up202007658@edu.fe.up.pt>
2023-11-11 14:41:03 +0000

    ev plot

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,4 +1,8 @@
 import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import PrecisionRecallDisplay
 
 LIMIT = 60
 PRECISION_AT = 20
@@ -48,7 +52,25 @@ def average_precision(results: list) -> float:
 def recall(results: list) -> float:
     return round(sum(recall_values(results)), 2)
 
+def precision_recall(results: list) -> None:
 
+    precision_results = precision_values(results)
+    recall_results = recall_values(results)
+    precision_recall_match = {k: v for k,v in zip(recall_results, precision_results)}
+
+    recall_results.extend([step for step in np.arange(0.1, 1.1, 0.1) if step not in recall_results])
+    recall_results = sorted(set(recall_results))
+
+    for idx, step in enumerate(recall_results):
+        if step not in precision_recall_match:
+            if recall_results[idx-1] in precision_recall_match:
+                precision_recall_match[step] = precision_recall_match[recall_results[idx-1]]
+            else:
+                precision_recall_match[step] = precision_recall_match[recall_results[idx+1]]
+
+    disp = PrecisionRecallDisplay([precision_recall_match.get(r) for r in recall_results], recall_results)
+    disp.plot()
+    plt.savefig('precision_recall.png')
 
 def evaluate(query: int, mode: str) -> None:
     results = getResults(query, mode)
@@ -56,6 +78,8 @@ def evaluate(query: int, mode: str) -> None:
     print(f"P@20: {precision_at_k(results)}")
     print(f"AvP: {average_precision(results)}")
     print(f"Recall: {recall(results)}")
+
+    precision_recall(results)
 
 if __name__ == "__main__":
 
