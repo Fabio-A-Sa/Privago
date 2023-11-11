ae610df3e553b2699f8494a2c6fd6e2d089f2c09
Fábio Sá <up202007658@edu.fe.up.pt>
2023-11-11 10:37:40 +0000

    compute structure

---
+++ evaluation/query.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+PARAMETERS = 'parameters.json'
+RESULT_SCHEMALESS = "result-schemaless.json"
+RESULT_BOOSTED = "result-boosted.json"
+
+def getParameters(query: int) -> json:
+    with open(PARAMETERS, 'r') as file:
+        data = json.load(file)
+        file.close()
+
+    return data.get(f'q{query}', {})
+
+def evaluation(query: int) -> None:
+    path = f"./q{query}"
+    print(getParameters(query))
+
+if __name__ == '__main__':
+    for query in range(1, 5):
+        evaluation(query)
