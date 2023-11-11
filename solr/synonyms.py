6e15408e3a1b943e7659369063870c19bfb122e3
Fábio Sá <up202007658@edu.fe.up.pt>
2023-11-11 17:47:28 +0000

    sys changes

--- solr/synonyms.py
+++ solr/synonyms.py
@@ -2,40 +2,46 @@ import pandas as pd
 import nltk
 from nltk import download
 from nltk.corpus import wordnet
+import os
 
+FILE_PATH = "../solr/synonyms_reviews.txt"
 
-nltk.download('averaged_perceptron_tagger', quiet=True)
-nltk.download('wordnet', quiet=True)
+def synonyms():
+    nltk.download('averaged_perceptron_tagger', quiet=True)
+    nltk.download('wordnet', quiet=True)
 
-file_path = f'../data/processed/hotels_complete.json'
-dataset = pd.read_json(file_path)
+    file_path = f'../data/processed/hotels_complete.json'
+    dataset = pd.read_json(file_path)
 
-all_texts = [review['text'] for reviews_list in dataset['reviews'] for review in reviews_list]
+    all_texts = [review['text'] for reviews_list in dataset['reviews'] for review in reviews_list]
 
-#print(all_texts)
+    word_dict = {}
+    words_checked = set()
 
-word_dict = {}
-words_checked = set()
+    for desc in all_texts:
+        if not isinstance(desc, str):
+            continue
 
-for desc in all_texts:
-    if not isinstance(desc, str):
-        continue
+        words = nltk.word_tokenize(desc)
+        nouns = [word.lower() for word, pos in nltk.pos_tag(words) if pos in ['NN', 'NNS'] and word.lower() not in words_checked]
 
-    words = nltk.word_tokenize(desc)
-    nouns = [word.lower() for word, pos in nltk.pos_tag(words) if pos in ['NN', 'NNS'] and word.lower() not in words_checked]
+        for noun in nouns:
+            synonyms = []
+            for syn in wordnet.synsets(noun):
+                for lm in syn.lemmas():
+                    if lm.name().lower() != noun:
+                        synonyms.append(lm.name())
+            if len(synonyms) > 1:
+                word_dict[noun] = set(synonyms)
+                words_checked.add(noun)
 
-    for noun in nouns:
-        synonyms = []
-        for syn in wordnet.synsets(noun):
-            for lm in syn.lemmas():
-                if lm.name().lower() != noun:
-                    synonyms.append(lm.name())  # adding into synonyms
-        if len(synonyms) > 1:
-            word_dict[noun] = set(synonyms)
-            words_checked.add(noun)
+    with open(FILE_PATH, 'w') as f:
+        for k, v in word_dict.items():
+            matches = list(v - {k})
+            list_synonyms = k + ", " + ", ".join(matches)
+            f.write(list_synonyms + "\n")
+        f.close()
 
-with open("synonyms.txt", 'w') as f:
-    for k, v in word_dict.items():
-        matches = list(v - {k})  # Exclude the original word from synonyms
-        list_synonyms = k + ", " + ", ".join(matches)
-        f.write(list_synonyms + "\n")
+if __name__ == '__main__':
+    if not os.path.exists(FILE_PATH):
+        synonyms()
