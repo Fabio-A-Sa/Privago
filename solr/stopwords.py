5048bbe79a48db2e89a1cf15fd8f838502075984
AvilaAndre <avila_andre14@hotmail.com>
2023-11-12 15:39:17 +0000

    stopwords

---
+++ solr/stopwords.py
@@ -0,0 +1,18 @@
+
+import nltk
+import os
+
+FILE_PATH = "../solr/stopwords.txt"
+
+def stopwords():
+
+    stop_words = nltk.corpus.stopwords.words('english')
+
+    with open(FILE_PATH, 'w') as f:
+        for word in stop_words:
+            f.write(word + "\n")
+        f.close()
+
+if __name__ == '__main__':
+    if not os.path.exists(FILE_PATH):
+        stopwords()
