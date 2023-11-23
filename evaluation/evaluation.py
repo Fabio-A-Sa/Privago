141e6951dc8d3c702671293900e845c121eabfbf
Fábio Sá <up202007658@edu.fe.up.pt>
2023-11-23 22:15:02 +0000

    new schemas and scripts

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,5 +1,4 @@
 import json
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import PrecisionRecallDisplay
---
+++ solr/startup-stopwords.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# To avoid problems with duplicated containers
+docker stop privago
+docker rm privago
+
+# Copy pipeline output to virtual "data" folder
+cp ../data/processed/hotels_complete.json .
+
+# Running Solr container
+# Creating "privago"
+# Creating a virtual folder "data"
+# Creating "hotels" core/collection
+docker run -p 8983:8983 --name privago -v "$(pwd)":/data -d solr:9.3 solr-precreate hotels
+sleep 10
+
+docker cp synonyms_reviews.txt privago:/var/solr/data/hotels/conf
+sleep 3
+
+docker cp stopwords.txt privago:/var/solr/data/hotels/conf
+sleep 3
+
+# Creating a schema based on "schema-boosted.json" file into "privago"
+curl -X POST -H 'Content-type:application/json' --data-binary "@./schema-stopwords.json" http://localhost:8983/solr/hotels/schema
+sleep 1
+
+# Populate collection using mapped folder "data" and pipeline output
+docker exec -it privago bin/post -c hotels -format solr /data/hotels_complete.json
+sleep 1
+
+# Removing always pipeline output for github/commits reasons
+rm hotels_complete.json
