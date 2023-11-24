9c18600f9eb9ce6df3cc425969779711478db78c
Fábio Sá <up202007658@edu.fe.up.pt>
2023-11-24 19:39:24 +0000

    save changes

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+
+function search() {
+    document.getElementById('results').innerHTML = 'Search results will appear here.';
+}
+
+function init() {
+    console.log("vai colocar eventlisteners em search bar")
+}
+
+init()
--- src/privago.js
+++ src/privago.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-
+const fs = require('fs');
+const port = process.argv[2] || 3000;
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+  
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    const html = fs.readFileSync('html/index.html', 'utf8');
+    res.send(html);
 });
-
+  
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
+    console.log('Configurations:', config);
 });
