4ab5e9bd4c3147d16ace7144045974acf7242055
Fábio Sá <up202007658@edu.fe.up.pt>
2023-11-25 11:04:06 +0000

    base

--- src/privago.js
+++ src/privago.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const fs = require('fs');
-const port = process.argv[2] || 3000;
+
+const cssPath = path.join(__dirname, 'css');
+const jsPath = path.join(__dirname, 'js');
+const htmlPath = path.join(__dirname, 'html');
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-  
+const port = process.argv[2] || 3000;
+
+app.use('/css', express.static(cssPath, { 'extensions': ['css'] }));
+app.use('/js', express.static(jsPath, { 'extensions': ['js'] }));
+app.use(express.static(htmlPath));
+
 app.get('/', (req, res) => {
-    const html = fs.readFileSync('html/index.html', 'utf8');
+    const html = fs.readFileSync(path.join(htmlPath, 'index.html'), 'utf8');
     res.send(html);
 });
-  
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
     console.log('Configurations:', config);
