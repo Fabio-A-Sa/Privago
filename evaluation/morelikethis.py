a6aeccc255bb1b18415e4d84b5c8edc25b44faa2
Fábio Sá <up202007658@edu.fe.up.pt>
2023-12-03 20:40:53 +0000

    save progress

--- evaluation/morelikethis.py
+++ evaluation/morelikethis.py
@@ -1,39 +1,49 @@
 import spacy
 import json
 
-nlp = spacy.load("en_core_web_sm")
-RESULTS_PATH = "./mlt/results.json"
+NPL = spacy.load("en_core_web_sm")
+SOLR_RESULTS_PATH = "./mlt/results.json"
+SOLR_EVALUATION_PATH = './mlt/evaluation.json'
 
 def calculate_similarity(review, similar_texts):
-    doc1 = nlp(review)
+    doc1 = NPL(review)
     similarities = []
 
     for text in similar_texts:
-        doc2 = nlp(text)
-        similarity = doc1.similarity(doc2)
+        doc2 = NPL(text)
+        similarity = round(doc1.similarity(doc2), 3)
         similarities.append(similarity)
 
     return similarities
 
-def print_results(entry, similarities):
-    print(f"Review: {entry['review']}")
-    print("Similarities:")
+def save_evaluation(evaluation):
+    
+    a = []
+    for (review, similiarities) in evaluation:
+        b = round(sum(similiarities) / len(similiarities), 2)
+        print(f"Partial: {b}")
+        a.append(b)
 
-    for i, similarity in enumerate(similarities, 1):
-        print(f"Result {i}: {similarity}")
-
-    print("\n")
+    print(f"Total: {round(sum(a) / len(a), 2)}")
 
 def main():
-    with open(RESULTS_PATH, 'r') as file:
-        results = json.load(file)
 
+    # Get solr results
+    results = []
+    with open(SOLR_RESULTS_PATH, 'r') as file:
+        results = json.load(file)
+        file.close()
+
+    # Evaluation based on similiarity
+    evaluation = []
     for entry in results:
         review = entry['review']
         similar_texts = entry['semelhantes']
         similarities = calculate_similarity(review, similar_texts)
+        evaluation.append([review, similarities])
 
-        print_results(entry, similarities)
+    # Save progress
+    save_evaluation(evaluation)
 
 if __name__ == "__main__":
     main()
